6de49d384c323dd69941f907e86112dd6054920f
fin <119663332+1f1n@users.noreply.github.com>
2024-06-16 14:09:24 +0100

    Update pnlCheck.py

--- pnlCheck.py
+++ pnlCheck.py
@@ -8,23 +8,49 @@ with open('top_traders.json', 'r') as file:
 def getMakerData(maker):
     url = f"https://gmgn.ai/defi/quotation/v1/smartmoney/sol/walletNew/{maker}?period=7d"
     response = requests.get(url)
+    print(f"[-] Dumping data..")
     if response.status_code == 200:
         makerData = response.json()
         if makerData['msg'] == "success":
             makerData = makerData['data']
             try:
-                return f"{maker} // RP: {makerData['realized_profit']:.2f} // URP: {makerData['unrealized_profit']:.2f} // 7d PnL: {makerData['pnl_7d'] * 100:.2f}%"
+                if makerData['pnl_7d'] is not None:
+                    unrealizedProfit = makerData['unrealized_profit']
+                    realizedProfit = makerData['realized_profit']
+                    unrealizedPnL = makerData['unrealized_pnl']
+                    weekPnL = makerData['pnl_7d'] * 100
+                    monthPnL = makerData['pnl_30d'] * 100
+                    winrate = makerData['winrate'] * 100
+
+                    return {
+                        "maker": maker,
+                        "realized_profit": realizedProfit,
+                        "unrealized_profit": unrealizedProfit,
+                        "unrealized_pnl": unrealizedPnL,
+                        "pnl_7d": weekPnL,
+                        "pnl_30d": monthPnL,
+                        "winrate": winrate
+                    }
             except TypeError:
-                return f"{maker} PnL is None"
-    return f"{maker} data fetch failed"
+                pass
+    return {"maker": maker, "error": "data fetch failed"}
 
 makers = [trader_info['maker'] for topTraders in data.values() for trader_info in topTraders.values()]
 
 results = []
 
-with ThreadPoolExecutor(max_workers=5) as executor: # edit max_workers based on your liking 
+with ThreadPoolExecutor(max_workers=5) as executor:
     futures = {executor.submit(getMakerData, maker): maker for maker in makers}
     for future in as_completed(futures):
         result = future.result()
-        results.append(result)
-        print(result)
+        if "error" not in result:
+            results.append(result)
+
+result_dict = {}
+for result in results:
+    maker = result.pop('maker')
+    result_dict[maker] = result
+
+with open('maker_data.json', 'w') as outfile:
+    json.dump(result_dict, outfile, indent=4)
+    print(f"[✅] Dumped profit data for {len(data)} wallet(s)..")
